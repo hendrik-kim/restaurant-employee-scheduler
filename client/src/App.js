e94620f872fd8ef361749e000106277f0f83df03
Hendrik Kim <94826294+leteatgo-mobile@users.noreply.github.com>
2022-05-01 15:45:01 -0500

    add hooks example

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [products, setProducts] = useState([
+    { name: 'product1', price: 100.0 },
+    { name: 'product2', price: 200.0 },
+  ]);
+
+  useEffect(() => {
+    fetch('https://localhost:5001/api/products')
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>Hook Example</h1>
+      <ul>
+        {products.map((item, index) => (
+          <li key={index}>
+            {item.name} - {item.price}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }

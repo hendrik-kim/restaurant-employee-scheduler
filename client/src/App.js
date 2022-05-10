ce268092071f49a742ac609c0e88c6f8e2fb7818
Hendrik Kim <94826294+leteatgo-mobile@users.noreply.github.com>
2022-05-09 23:33:08 -0500

    add login screen

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import HomeScreen from './screens/HomeScreen';
+import LoginScreen from './screens/LoginScreen';
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<HomeScreen />} exact />
+        {/* <Route path='/' element={<HomeScreen />} exact /> */}
+        <Route path='/' element={<LoginScreen />} exact />
       </Routes>
     </Router>
   );
